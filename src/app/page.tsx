36109d691215252022192c7155a4865990900900
____MAX____ <soldatkin.maks2018@yandex.ru>
2025-04-25 19:40:39 +0300

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,14 @@ import styles from './page.module.css'
 export default function Home() {
 	return (
 		<main className={styles.main}>
+			<h1>Я КУБИК</h1>
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
+			<br />
 			<span className={styles.square}></span>
 		</main>
 	)
